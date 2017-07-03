0c19df4f823ad4cb16d518217195bbed5e31c417
apa yu <rx836@hotmail.com>
2017-07-03 19:37:13 +0800

    todo list

---
+++ src/js/dispatcher/AppDispatcher.js
@@ -0,0 +1,13 @@
+import { Dispatcher } from "flux";
+
+class DispatcherClass extends Dispatcher {
+  handleAction(action){
+    this.dispatch({
+      type: action.type,
+      payload: action.payload,
+    });
+  }
+}
+
+const AppDispatcher = new DispatcherClass();
+export default AppDispatcher;
