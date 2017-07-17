8e68fad8b4900fa836bcace679dd46af8561e94c
apa yu <rx836@hotmail.com>
2017-07-17 16:29:07 +0800

    build directory

--- src/js/index.js
+++
@@ -1,4 +0,0 @@
-import React from 'react';
-import { render } from 'react-dom';
-
-render(<p>Hello</p>, document.getElementById('app'));
--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,8 @@
-const path = require('path');
-const webpack = require('webpack');
+const path = require('path')
+const webpack = require('webpack')
 
 module.exports = {
-  entry: './src/js/index.js',
+  entry: './src/index.js',
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist'),
