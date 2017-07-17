99b5a367972c041ae14d425071c2a0d63883499a
apa yu <rx836@hotmail.com>
2017-07-17 16:59:58 +0800

    install flow

--- src/index.js
+++ src/index.js
@@ -1,4 +1,15 @@
+// @flow
 import React from 'react'
 import { render } from 'react-dom'
+
+function foo(x: mixed): string {
+  if (typeof x === 'number' || typeof x === 'string') {
+    return `${x} + 10`
+  }
+  throw new Error('Invalid x type')
+}
+
+foo('Hello!')
+foo(1)
 
 render(<p>Hello</p>, document.getElementById('app'))
